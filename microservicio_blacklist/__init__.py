3be6f63934662c4a6891661b77e0bc656e3e1abc
Angelica <am.rodriguez12@uniandes.edu.co>
2022-04-05 21:28:42 -0500

    Se configura la db

--- microservicio_blacklist/__init__.py
+++ microservicio_blacklist/__init__.py
@@ -2,8 +2,7 @@ from flask import Flask
 
 def create_app(config_name):
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blacklist.db'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://proyecto:proyecto@aa1dwcgg1jsrmhk.c6bctyrz9gs4.us-east-1.rds.amazonaws.com/ebdb'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
     
     return app
