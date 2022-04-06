b09e446a37a83333f76d969566abe6d34f0ecfab
Angelica <am.rodriguez12@uniandes.edu.co>
2022-04-06 18:04:14 -0500

    Se cambia el nombre de app por application

--- microservicio_blacklist/__init__.py
+++ microservicio_blacklist/__init__.py
@@ -1,8 +1,9 @@
+from email.mime import application
 from flask import Flask
 
 def create_app(config_name):
-    app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://proyecto:proyecto@aa1dwcgg1jsrmhk.c6bctyrz9gs4.us-east-1.rds.amazonaws.com/ebdb'
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    application = Flask(__name__)
+    application.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://proyecto:proyecto@aa1dwcgg1jsrmhk.c6bctyrz9gs4.us-east-1.rds.amazonaws.com/ebdb'
+    application.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     
-    return app
+    return application
